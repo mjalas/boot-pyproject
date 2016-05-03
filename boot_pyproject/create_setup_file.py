1bfa6164055053005d902938e38f3c2420ecb7c5
Mats Jalas <mats.jalas@gmail.com>
2016-05-03 22:55:39 +0300

    Added functionality to create the project config file in yaml format.

--- boot_pyproject/create_setup_file.py
+++ boot_pyproject/create_setup_file.py
@@ -1,9 +1,37 @@
 import json
+import yaml
 
 
 class SetupTemplateBuilder(object):
 
     @staticmethod
-    def build():
-        setup_data = {}
-        return json.dumps(setup_data)
+    def create_setup_data():
+        setup_data = {
+            "metadata": {
+                "NAME": "",
+                "VERSION": "",
+                "DESCRIPTION": "",
+                "URL": "",
+                "AUTHOR": "",
+                "AUTHOR_EMAIL": "",
+                "LICENSE": "",
+                "KEYWORDS": "",
+                "CLASSIFIERS": [],
+                "EXCLUDE": [],
+                "SETUP_REQUIRES": [],
+                "TEST_SUITE": "nose.collector",
+                "TESTS_REQUIRES": ["nose"],
+                "INSTALL_REQUIRES": []
+            }
+        }
+        return setup_data
+
+    @staticmethod
+    def build_json():
+        result = SetupTemplateBuilder.create_setup_data()
+        return json.dumps(result)
+
+    @staticmethod
+    def build_yaml():
+        result = SetupTemplateBuilder.create_setup_data()
+        return yaml.dump(result)
--- boot_pyproject/tests/test_create_setup_file.py
+++ boot_pyproject/tests/test_create_setup_file.py
@@ -1,11 +1,39 @@
 from unittest import TestCase
 from boot_pyproject.create_setup_file import SetupTemplateBuilder
 import json
+import yaml
 
 
 class TestSetupTemplateBuilder(TestCase):
 
-    def test_create_setup_file(self):
-        expected = {}
-        result = SetupTemplateBuilder.build()
-        self.assertEqual(expected, json.loads(result))
+    def setUp(self):
+        self.expected_dict = {
+            "metadata": {
+                "NAME": "",
+                "VERSION": "",
+                "DESCRIPTION": "",
+                "URL": "",
+                "AUTHOR": "",
+                "AUTHOR_EMAIL": "",
+                "LICENSE": "",
+                "KEYWORDS": "",
+                "CLASSIFIERS": [],
+                "EXCLUDE": [],
+                "SETUP_REQUIRES": [],
+                "TEST_SUITE": "nose.collector",
+                "TESTS_REQUIRES": ["nose"],
+                "INSTALL_REQUIRES": []
+            }
+        }
+
+    def test_create_setup_data(self):
+        result = SetupTemplateBuilder.create_setup_data()
+        self.assertEqual(self.expected_dict, result)
+
+    def test_create_setup_json(self):
+        result = SetupTemplateBuilder.build_json()
+        self.assertEqual(json.dumps(self.expected_dict), result)
+
+    def test_create_setup_yaml(self):
+        result = SetupTemplateBuilder.build_yaml()
+        self.assertEqual(yaml.dump(self.expected_dict), result)
