d43116aa15f0879c03e4647aa4b6b4d4ef86586e
Mats Jalas <mats.jalas@gmail.com>
2016-05-03 23:10:07 +0300

    Removed setup method

--- boot_pyproject/app.py
+++ boot_pyproject/app.py
@@ -2,8 +2,5 @@
 
 class App(object):
 
-    def setup(self):
-        pass
-
     def run(self):
         pass
--- boot_pyproject/tests/test_app.py
+++ boot_pyproject/tests/test_app.py
@@ -4,9 +4,6 @@ from boot_pyproject.app import App
 
 class TestApp(TestCase):
 
-    def test_setup(self):
-        app = App()
-
     def test_run(self):
         app = App()
         app.run()
