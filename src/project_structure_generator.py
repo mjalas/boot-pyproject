52e2243957a9bcb99f67b65e3ab9e661a0ac2dff
Mats Jalas <mats.jalas@gmail.com>
2017-02-08 21:22:17 +0200

    Implemented missing python package init files for src and tests.

--- src/project_structure_generator.py
+++ src/project_structure_generator.py
@@ -1,12 +1,18 @@
-import os
+"""Project structure generator module."""
+from pathlib import Path
 from src.file_folder_creators import create_folder
 
 class ProjectStructureGenerator(object):
+    """Project structure generator class."""
 
     @staticmethod
     def generate(root_path, project_name):
         """Generates the project folder structure."""
-        project_base = root_path + "/" + project_name
+        project_base = root_path + '/' + project_name
+        src_path = project_base + '/src'
+        tests_path = project_base + '/tests'
         create_folder(project_base)
-        create_folder(project_base + "/src")
-        create_folder(project_base + "/tests")
+        create_folder(src_path)
+        Path(src_path + '/__init__.py').touch()
+        create_folder(tests_path)
+        Path(tests_path + '/__init__.py').touch()
